946ba58a885a442f563f108b942044ef809c54f0
vkatariya <vkatariya1991@gmail.com>
2018-06-27 16:38:05 +0530

    this is upload a first project

---
+++ application/controllers/Upload_controller.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Upload_controller extends CI_Controller {
+	 function __construct()
+	 {
+		 parent::__construct();
+		  
+	 }
+     	 
+}
